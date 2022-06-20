11809874d8b8d787e3e5df38013e28a8001cc94b
Pab-lo <salaberry.pablo@gmail.com>
2022-06-20 14:16:18 +0200

    Just a simple static page to try some things

--- src/index.js
+++ src/index.js
@@ -1,17 +1,51 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react"
+import ReactDOM from "react-dom"
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function Header() {
+    return (
+        <header>
+            <nav className="nav">
+                <img src="./top-gun-maverick.png" className="nav-logo" />
+                <ul className="nav-items">
+                    <li>Tickets</li>
+                    <li>Bio</li>
+                    <li>Contact</li>
+                </ul>
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+                
+            </nav>
+        </header>
+    )
+}
+
+function Footer() {
+    return (
+        <footer>
+            <small>© 2021 Pab development. All rights reserved.</small>
+        </footer>
+    )
+}
+
+function MainContent() {
+    return (
+        <div>
+            <h1>Top Gun Maverick</h1>
+            <ol>
+                <li>With Tom Cruise</li>
+                <li>One of the best movies of 2022</li>
+            </ol>
+        </div>
+    )
+}
+
+function Page() {
+    return (
+        <div>
+            <Header />
+            <MainContent />
+            <Footer />
+        </div>
+    )
+}
+
+ReactDOM.render(<Page />, document.getElementById("root"))
